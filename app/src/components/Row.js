26f30008edd6eea6a337d680940a709d4517a590
yeongjae <yeongjae9501@gmail.com>
2022-12-05 09:38:02 +0900

    movie trailer 적용 완료

--- app/src/components/Row.js
+++ app/src/components/Row.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './css/Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
   const base_URL = 'https://image.tmdb.org/t/p/original/';
 
   useEffect(() => {
@@ -20,6 +23,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log('url', urlParams);
+          setTrailerUrl(urlParams.get('v'));
+        })
+
+        .catch((error) => console.log(error));
+    }
+    console.log('trailerurl', trailerUrl);
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -32,11 +58,15 @@ function Row({ title, fetchUrl, isLargeRow }) {
               src={`${base_URL}${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
-              alt={movie.title}
+              onClick={() => {
+                handleClick(movie);
+              }}
+              alt={movie.name}
             />
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
